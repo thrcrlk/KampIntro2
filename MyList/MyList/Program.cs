4b710c83bb507809e5c1654ebeb5b06594686d16
thrcrlk <77546899+thrcrlk@users.noreply.github.com>
2021-01-24 13:22:03 +0300

    myList Generic Class

---
+++ MyList/MyList/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MyList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> myList = new MyList<string>();
+            myList.Add("asdasd");
+            myList.Add("asdasd");
+            myList.Add("asdasd");
+
+            Console.WriteLine(myList.Length);
+            //foreach (var x in myList.Items)
+            //{
+            //    Console.WriteLine(x);
+            //}
+            //Console.WriteLine(myList.Items[myList.Length-1]); 
+        }
+    }
+}
